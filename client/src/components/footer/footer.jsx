cda1256576d84286ea4f15b9f4bf2f7a33c9de20
Justin Sykes <atlsykes1@att.net>
2021-01-26 21:10:57 -0500

    added footer component

---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const footer = () => {
+    return (
+        <div>
+        <footer class="page-footer teal lighten-4">
+          <div class="footer-copyright teal lighten-3">
+            <div class="container">
+            © 2021 Copyright Justin Sykes
+            </div>
+          </div>
+        </footer>
+        </div>
+    );
+};
+
+export default footer;
--- client/src/containers/home/home.jsx
+++ client/src/containers/home/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../components/navbar/navbar';
 import Homecard from '../../components/card/homecard'
+import Footer from '../../components/footer/footer'
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
             <Homecard/>
+            <Footer/>
         </div>
     );
 };
