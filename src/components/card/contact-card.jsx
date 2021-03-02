1369bb6a84add6e00cbd43966f42a5304a220279
Justin Sykes <atlsykes1@att.net>
2021-03-02 09:39:32 -0500

    fixed form to email all fields and have field input validation

--- src/components/card/contact-card.jsx
+++ src/components/card/contact-card.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const Contactcard = () => {
+  //form action on submit
+  function submitForm (ev) {
+    alert(
+      'Your message has been sent.  Please allow 24-48 business hours for a response.'
+    )
+  }
+
   return (
     <div>
       <br />
@@ -10,21 +17,40 @@ const Contactcard = () => {
         </div>
         <br />
         <div className='row'>
-          <form className='col s11' action="https://formspree.io/f/xaylkwgy" method="POST">
+          <form
+            className='col s11'
+            action='https://formspree.io/f/xaylkwgy'
+            method='POST'
+          >
             <h6>Full Name</h6>
             <div className='input-field col s11'>
               <i className='material-icons prefix'>account_circle</i>
-              <input id='icon_prefix' type='text' className='validate' />
+              <input
+                id='fullname'
+                type='text'
+                className='validate'
+                name='full-name'
+                required
+              />
               <h6>Email</h6>
               <div className='input-field col s11'>
                 <i className='material-icons prefix'>email</i>
-                <input id='icon_email' type='email' className='validate' />
+                <input
+                  id='icon_email'
+                  type='email'
+                  className='validate'
+                  name='email'
+                  required
+                />
                 <h6>Message</h6>
                 <div className='input-field col s12'>
                   <i className='material-icons prefix'>mode_edit</i>
                   <textarea
                     id='icon_prefix2'
                     className='materialize-textarea'
+                    type='text'
+                    name='message'
+                    required
                   ></textarea>
                   <br />
                   <br />
@@ -33,7 +59,8 @@ const Contactcard = () => {
                       className='btn waves-effect waves-light'
                       type='submit'
                       name='action'
-                      id='submit-button'
+                      value='Send'
+                      onClick={submitForm}
                     >
                       Submit
                       <i className='material-icons right'>send</i>
